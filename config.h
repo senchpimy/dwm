f353563d9b0395c497eb9dec08fa2ef2570bfea1
senchi <senchpimy@gmail.com>
2022-10-03 11:32:40 -0500

    status2d

--- config.h
+++ config.h
@@ -47,7 +47,7 @@ static const Rule rules[] = {
 	{ "TelegramDesktop",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "obs",                NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Lutris",             NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "St",                 NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "St",                 NULL,     NULL,           0,         0,          1,           1,        -1 },
 	{ NULL,                 NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
